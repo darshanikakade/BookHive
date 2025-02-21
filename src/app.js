b5211c8b056fbb8627da00122903b2d67f34465c
darshanik <kakadedarshani@gmail.com>
2025-02-21 12:39:14 +0530

    initializing sequelize orm and dotenv

--- src/app.js
+++ src/app.js
@@ -1,12 +1,36 @@
 const express = require('express')
+const { Sequelize } = require('sequelize');
+require('dotenv').config()
+
 const app = express()
+const port = process.env.PORT
 
-const port = 3000
+const dbName = process.env.DB_NAME
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+const dbHost = process.env.DB_HOST
 
-app.get("/", (req,res)=>{
+
+app.get("/", (req, res) => {
     res.send("Hello World..!")
 })
 
-app.listen(port, ()=>{
-    console.log('Server is running on port ', port);
-})
+
+const sequelize = new Sequelize(
+    dbName,
+    dbUser,
+    dbPassword,
+    {
+        host: dbHost,
+        dialect: 'mysql'
+    }
+);
+
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+    app.listen(port, async () => {
+        console.log('Server is running on port ', port);
+    })
+}).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+});
